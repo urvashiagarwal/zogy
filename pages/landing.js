1dd0928adcc3c5d87cc140d9454bfa2155e9e020
Urvashi Agarwal <urvashiagarwal2019@gmail.com>
2023-07-18 22:24:08 +0530

    aa

--- pages/landing.js
+++ pages/landing.js
@@ -1,10 +1,29 @@
+import { HeroMenu } from '@/Home/HeroMenu'
 import LandingBanners from '@/Landing/LandBanner'
 import React from 'react'
 
 const Landing = () => {
+
+    const data = {
+        value: "white",
+        textcolor: "black",
+        text4clr: "black",
+        margin: "calc(-40px)",
+        clr: "black",
+        imgheight: "295px",
+        imgwidth: "336px",
+        imgpadding: "270px",
+        margintop: "calc(-300px)",
+        marginbtw: "100px",
+        marginleft: "calc(-0px)",
+        topgap: "calc(-70px)",
+        marginbtm: "calc(-280px)"
+
+    }
     return (
         <>
             <LandingBanners />
+            <HeroMenu {...data} />
 
 
         </>
