b43959e50a333f02281faa117038b2952b163a23
Urvashi Agarwal <urvashiagarwal2019@gmail.com>
2023-07-18 12:03:06 +0530

    done

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import { Inter } from 'next/font/google'
 
 import { Header } from '@/components/Header';
 import { HeroHome } from '@/components/HeroHome';
-import Landing from './Landing';
-
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,10 +11,10 @@ export default function Home() {
   return (
     <>
       <Header />
-     
-    <HeroHome />
-    
-     
+
+      <HeroHome />
+
+
 
     </>
   )
