56eccfdc5047d929cf54394a34809c750ee5ad08
Urvashi Agarwal <urvashiagarwal2019@gmail.com>
2023-07-26 19:44:09 +0530

    aa

--- Home/HeroBanner.js
+++ Home/HeroBanner.js
@@ -5,11 +5,11 @@ export const HeroBanner = () => {
                 <div className="container-fluid banner">
                     <div className="row">
 
-                        <div className="col col-lg-1">
+                        <div className="col col-lg-1 col-md-0 col-sm-0">
 
                         </div>
 
-                        <div className=" col col-lg-5 col-md-6 col-sm-10 lunch ">
+                        <div className=" col col-lg-5 col-md-6 col-sm-12 lunch ">
                             <div id="circle">
                                 <div>
                                     <h2>40%</h2>
@@ -22,7 +22,7 @@ export const HeroBanner = () => {
                             <button>Read More</button>
                         </div>
 
-                        <div className="col col-lg-6 col-md-6 col-sm-2">
+                        <div className="col col-lg-6 col-md-6 col-sm-0">
 
                         </div>
 
