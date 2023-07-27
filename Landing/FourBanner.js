5992c53d9030211b5c73ebd3165d379f7428c999
Urvashi Agarwal <urvashiagarwal2019@gmail.com>
2023-07-27 15:30:21 +0530

    aa

--- Landing/FourBanner.js
+++ Landing/FourBanner.js
@@ -42,7 +42,7 @@ export const FourBanner = () => {
                     </div>
                 </div>
                 <div className="h-box">
-                    <div className="row">
+                    <div className="row" style={{ display: "flex" }}>
 
                         <div class="col col-2 msg">
                             <img
