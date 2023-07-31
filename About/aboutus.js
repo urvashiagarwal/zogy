8ee6b31cf16a06394b6ff0c2dde11fc9977b20e1
Urvashi Agarwal <urvashiagarwal2019@gmail.com>
2023-07-31 22:19:40 +0530

    ad

--- About/aboutus.js
+++ About/aboutus.js
@@ -1,7 +1,7 @@
-export const AboutUs =()=>{
-    return(
+export const AboutUs = () => {
+    return (
         <>
-            <div className="container-fluid">
+            <div className="container-fluid about">
                 <div className="row">
                     <div className="col col-lg-1">
 
@@ -9,14 +9,17 @@ export const AboutUs =()=>{
                     <div className="col col-lg-5 mission">
                         <h4>
                             OUR MISSION
-                            </h4>
+                        </h4>
                         <h1>About Us</h1>
                         <h5>Article evident arrived express highest men did boy. Mistress sensible entirely am so. Quick can manor smart money hopes worth too. Comfort produce husband boy her had hearing. Law others theirs passed but wishes. You day real less till dear read. Considered use dispatched melancholy sympathize discretion led. Oh feel if up to till like.</h5>
                         <h5>Image from <span>Freepik</span> </h5>
                         <button>Read More</button>
                     </div>
                     <div className="col col-lg-5 f-img">
-
+                        <img src="https://assets.nicepagecdn.com/11a8ddce/4267640/images/juicy-steak-medium-rare-beef-with-spices-grilled-vegetables.jpg" alt="" style={{
+                            width: "540px",
+                            height: "521px"
+                        }} />
                     </div>
                     <div className="col col-lg-1">
 
