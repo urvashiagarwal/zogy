6fa08f23969dfb765434ed1becd6f8576150378c
Urvashi Agarwal <urvashiagarwal2019@gmail.com>
2023-07-31 22:14:04 +0530

    hhjbk

---
+++ About/aboutus.js
@@ -0,0 +1,28 @@
+export const AboutUs =()=>{
+    return(
+        <>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col col-lg-1">
+
+                    </div>
+                    <div className="col col-lg-5 mission">
+                        <h4>
+                            OUR MISSION
+                            </h4>
+                        <h1>About Us</h1>
+                        <h5>Article evident arrived express highest men did boy. Mistress sensible entirely am so. Quick can manor smart money hopes worth too. Comfort produce husband boy her had hearing. Law others theirs passed but wishes. You day real less till dear read. Considered use dispatched melancholy sympathize discretion led. Oh feel if up to till like.</h5>
+                        <h5>Image from <span>Freepik</span> </h5>
+                        <button>Read More</button>
+                    </div>
+                    <div className="col col-lg-5 f-img">
+
+                    </div>
+                    <div className="col col-lg-1">
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import { AboutUs } from "@/About/aboutus"
+
+const About = () => {
+    return (
+        <>
+            <AboutUs />
+        </>
+    )
+}
+
+export default About
